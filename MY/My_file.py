e7d05acf7250f5c7d7cbcb3b72d4f9aa20c204ef
Ter4ikds <guzelpan4era@gmail.com>
2023-01-05 23:52:44 +0500

    del

---
+++ MY/My_file.py
@@ -0,0 +1,6 @@
+age = 26
+name = 'Swaroop'
+print('Возраст {0} -- {1} лет.'.format(name, age))
+print('Почему {0} забавляется с этим Python?'.format(name))
+# Возраст Swaroop -- 26 лет.
+# Почему Swaroop забавляется с этим Python?
