8e9f1467e08908a9b31eb1d30b8c24e98b72953c
Ter4ikds <guzelpan4era@gmail.com>
2023-01-11 00:59:19 +0500

    initial command

--- Project_1/game_v3.py
+++ Project_1/game_v3.py
@@ -1,4 +1,4 @@
-"""Игра угадай число"""
+"""Игра угадай число меньше чем за 20 попыток"""
 
 import numpy as np
 
@@ -7,8 +7,8 @@ def random_predict(number: int=np.random.randint(1, 101)) -> int:
     min = 1
     max = 100
 
-    predict_number = int(np.random.randint(min, max + 1))  #программа начинает отгадывать число
-    count = 0  #подсчет количества попыток на отгадывание числа
+    predict_number = int(np.random.randint(min, max + 1))  #  программа начинает отгадывать число
+    count = 0  # подсчет количества попыток на отгадывание числа
 
     while predict_number != number:
         count += 1
@@ -22,15 +22,15 @@ def random_predict(number: int=np.random.randint(1, 101)) -> int:
             predict_number = (min + max) // 2
 
         else:
-            break  #конец игры и выход из цикла
-    #print(f"Робот угадал число! Это число = {number}, за {count} попыток.")
+            break  # конец игры и выход из цикла
+    
     return count
 
-#print(random_predict(2))
+#  print(random_predict(2))
 def score_game(random_predict) -> int:
     count_ls = []
     np.random.seed(np.random.randint(1, 10)) # не фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(100)) #загадали список рандомных чисел
+    random_array = np.random.randint(1, 101, size=(100)) #  загадали список рандомных чисел
     for number in random_array:
         count_ls.append(random_predict(number))
     score = int(np.mean(count_ls))
